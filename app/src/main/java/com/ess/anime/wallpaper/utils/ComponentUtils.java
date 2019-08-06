f2a1a03b546331b493e98baf1dcea24f7f339bbd
EternalSoySauce <1018717197@qq.com>
2019-08-06 12:05:38 +0800

    v1.7.1 自定义裁剪壁纸

--- app/src/main/java/com/ess/anime/wallpaper/utils/ComponentUtils.java
+++ app/src/main/java/com/ess/anime/wallpaper/utils/ComponentUtils.java
@@ -167,9 +167,9 @@ public class ComponentUtils {
     public static Intent installApk(Context context, File apkFile, boolean start) {
         try {
             Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 Uri contentUri = FileProvider.getUriForFile(context, getFileProviderAuthority(context), apkFile);
                 intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
             } else {
