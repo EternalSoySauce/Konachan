b3c8c41d62cbee163d157e19022ec697bd4db66d
EternalSoySauce <1018717197@qq.com>
2019-11-08 21:24:43 +0800

    引入bugly

--- app/src/main/java/com/ess/anime/wallpaper/MyApp.java
+++ app/src/main/java/com/ess/anime/wallpaper/MyApp.java
@@ -8,14 +8,21 @@ import android.preference.PreferenceManager;
 
 import com.ess.anime.wallpaper.global.Constants;
 import com.ess.anime.wallpaper.http.OkHttp;
+import com.tencent.bugly.crashreport.CrashReport;
 
 import androidx.multidex.MultiDex;
 
 public class MyApp extends Application {
 
+    private final static String BUGLY_APP_ID = "82daf92318";
+
+    private static MyApp sApplication;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        sApplication = this;
+        CrashReport.initCrashReport(this, BUGLY_APP_ID, false);
         StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().build());
         OkHttp.initHttpConfig(this);
         initData();
@@ -24,6 +31,10 @@ public class MyApp extends Application {
     private void initData() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         Constants.sAllowPlaySound = preferences.getBoolean(Constants.ALLOW_PLAY_SOUND, true);
+    }
+
+    public static MyApp getInstance() {
+        return sApplication;
     }
 
     @Override
